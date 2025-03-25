ea0105acf3256ff3c2044db4386e6c9b3ec7c197
Andrey <112519966+ahahaharu@users.noreply.github.com>
2025-03-25 12:56:26 +0300

    Settings added

---
+++ Lab2_console-textEditor-app/src/classes/User/User.js
@@ -0,0 +1,15 @@
+class User {
+  constructor(name, role) {
+    this.name = name;
+    this.role = role;
+  }
+  getRole() {
+    return this.role;
+  }
+  setRole(role) {
+    this.role = role;
+    this.notifyObservers();
+  }
+}
+
+module.exports = { User };
